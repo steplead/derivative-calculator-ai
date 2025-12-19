270386f0e2bf65eceec1b4e31bdfe913b85d27ac
steplead <aquamo@gmail.com>
2025-12-19 09:40:25 +0800

    chore(config): Disable image optimization for Vercel free tier

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   rewrites: async () => {
     return [
       {

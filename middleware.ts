308f7385e0337566c6c08564c53671f8af5f2521
steplead <aquamo@gmail.com>
2025-12-21 22:32:32 +0800

    chore: temporarily disable dynamic manifest to isolate crash

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const runtime = 'experimental-edge';
+export const runtime = 'edge';
 
 export function middleware(request: NextRequest) {
     return NextResponse.next();
